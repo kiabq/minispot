816427933095a9027a81707988274d9ee0d474f8
kiabq <conway.joseph@pm.me>
2023-05-09 17:56:09 -0400

    update: add lifespan function and refresh token functions
    - The lifespan function will take the initial token provided from environment variables and generate the first access token
    
    - The refresh token function will take the refresh token that is generated on initial launch and refresh the access token once it expires

--- server/api.py
+++ server/api.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from requests import get, post, head
 from dotenv import dotenv_values
 import json
 
-app = FastAPI()
+ACCESS_TOKEN = None
+REFRESH_TOKEN = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global ACCESS_TOKEN
+    global REFRESH_TOKEN
+
+    tokens = await create_initial_token()
+
+    ACCESS_TOKEN = tokens["access_token"]
+    REFRESH_TOKEN = tokens["refresh_token"]
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost",
@@ -14,48 +30,57 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    # TODO:
-    # - Set access_token and refresh_token after first call
-    # using an initial code.
-    # - Create a method (or middleware maybe?) that will update the access_token
-    # (using the refresh_token) once it has expired
-    tokens = await create_initial_token()
-    access_token = tokens['access_token']
-    refresh_token = tokens['refresh_token']
-
     request_url = "https://api.spotify.com/v1/me/player/currently-playing"
-    request_headers = { "Authorization": f"Bearer {access_token}" }
-    val = get(request_url, headers=request_headers)
+    request_headers = { "Authorization": f"Bearer {ACCESS_TOKEN}" }
+    data = get(request_url, headers=request_headers)
 
-    return {
-        "message": val.json()
-    }
+    if (data.status_code == 200):
+        return {
+            "data": data.json()
+        }
+    else:
+        refresh_token()
 
 async def refresh_token():
+    grant_type = "refresh_token"
+    client_id = dotenv_values(".env")["client_id"]
+    client_secret = dotenv_values(".env")["client_secret"]
+    request_url = f"https://accounts.spotify.com/api/token?grant_type={grant_type}&refresh_token={REFRESH_TOKEN}&client_id={client_id}"
+    request_headers = { 
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": f"Basic {client_id}:{client_secret}" 
+    }
+    
+    data = post(request_url, headers=request_headers)
+
+    if (data.status_code):
+        global ACCESS_TOKEN
+        ACCESS_TOKEN = data["access_token"]
+
     return None
 
 async def create_initial_token():
     callback_uri = "https://accounts.spotify.com/api/token"
-    client_id = dotenv_values(".env")["client_id"]
     grant_type = "authorization_code"
+    client_id = dotenv_values(".env")["client_id"]
     initial_code = dotenv_values(".env")["initial_code"]
     redirect_uri = dotenv_values(".env")["redirect_uri"]
-    code_verifier = "code"
     client_secret = dotenv_values(".env")["client_secret"]
-
+    code_verifier = "code"
+    
     url = f"{callback_uri}?client_id={client_id}&grant_type={grant_type}&code={initial_code}&redirect_uri={redirect_uri}&code_verifier={code_verifier}&client_secret={client_secret}"
     request_headers = { "Content-Type": "application/x-www-form-urlencoded" }
     
     tokens = post(url, headers=request_headers)
 
-    if (tokens.status_code == 200) :
+    if (tokens.status_code == 200):
         return tokens.json()
     else:
-        raise Exception(f"Unexpected response from server: {tokens.status_code}")
+        raise Exception(f"Initial token was not accepted: {tokens.status_code}")
