615f55acf7edf87908435585be69919425f391b2
kiabq <conway.joseph@pm.me>
2023-05-09 19:05:02 -0400

    fix: change credentials from plain text to base64

--- server/api.py
+++ server/api.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from requests import get, post, head
 from dotenv import dotenv_values
+from base64 import b64encode
 import json
 
 ACCESS_TOKEN = None
@@ -46,23 +47,27 @@ async def read_root():
             "data": data.json()
         }
     else:
-        refresh_token()
+        await refresh_token()
 
 async def refresh_token():
     grant_type = "refresh_token"
     client_id = dotenv_values(".env")["client_id"]
     client_secret = dotenv_values(".env")["client_secret"]
     request_url = f"https://accounts.spotify.com/api/token?grant_type={grant_type}&refresh_token={REFRESH_TOKEN}&client_id={client_id}"
+    
+    credentials = f"{client_id}:{client_secret}"
+    credentials_b64 = b64encode(credentials.encode("ascii")).decode("ascii")
+
     request_headers = { 
         "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": f"Basic {client_id}:{client_secret}" 
+        "Authorization": f"Basic {credentials_b64}" 
     }
     
     data = post(request_url, headers=request_headers)
 
-    if (data.status_code):
+    if (data.status_code == 200):
         global ACCESS_TOKEN
-        ACCESS_TOKEN = data["access_token"]
+        ACCESS_TOKEN = data.json()["access_token"]
 
     return None
 
